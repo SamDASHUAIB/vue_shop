bcd2263c75643fec6e7a7f3e709d78a7b0383d74
sam <2324831337@qq.com>
2020-08-24 18:43:00 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表(global.css)
+import './assets/css/global.css'
+// 导入字体图片
+import './assets/fonts/iconfont.css'
 
+// axios
+import axios from 'axios'
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Input, Form, FormItem, Message } from 'element-ui'
 
+// 按需导入所需配件, 先导入, 后使用
 Vue.use(Button)
+Vue.use(Input)
+Vue.use(Form)
+Vue.use(FormItem)
+// 全局挂载(挂载到 vue 的原型对象上)
+Vue.prototype.$message = Message
